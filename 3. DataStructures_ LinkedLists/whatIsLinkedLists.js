537bb1523f9d6a2b04a18fc15655e94afe090054
Artem6evelev <artem6evelev92@gmail.com>
2022-06-04 10:55:39 +0300

    repeat linked lists data structure from the beginning

---
+++ 3. DataStructures_ LinkedLists/whatIsLinkedLists.js
@@ -0,0 +1,10 @@
+const basket = ["apples", "grapes", "pears"]
+//? How we can have a basket that is not an array, but linked list?
+
+//  linked list : apples --> grapes --> pears
+// --> arrow is pointers to next one
+
+apples
+8947 --> grapes
+          8742 --> pears
+                    372 --> null
