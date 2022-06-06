a0ea58f1a47823a7f1595210856a31451e95f23e
Artem6evelev <artem6evelev92@gmail.com>
2022-06-06 10:00:12 +0300

    async fetch refresh

---
+++ AsyncJS/fetch.js
@@ -0,0 +1,37 @@
+const delay = (ms) => {
+  return new Promise((r) => setTimeout(() => r(), ms));
+};
+
+delay(2000).then(() => console.log("2 sec"));
+
+const url = "https://jsonplaceholder.typicode.com/todos";
+
+// function fetchTodos() {
+//   console.log("Fetch todo started");
+//   return delay(2000)
+//     .then(() => fetch(url))
+//     .then((response) => response.json());
+// }
+
+// fetchTodos()
+//   .then((data) => {
+//     console.log("Data", data);
+//   })
+//   .catch((e) => console.error(e));
+
+//* async await
+async function fetchAsyncTodos() {
+  console.log("Fetch todo started");
+  try {
+    await delay(2000); // resolve -->
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log("Data", data);
+  } catch (err) {
+    console.log("Error", err);
+  } finally {
+    // for example close dialog
+  }
+}
+
+fetchAsyncTodos();
