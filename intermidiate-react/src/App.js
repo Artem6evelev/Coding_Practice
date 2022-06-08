2437ee0ecdfb2898a4760fe1c080c8b5ccbe0f21
Artem6evelev <artem6evelev92@gmail.com>
2022-06-08 13:16:22 +0300

    finish with paractising react, start codewars

--- intermidiate-react/src/App.js
+++ intermidiate-react/src/App.js
@@ -1,7 +1,41 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+//TODO :
+// 1. Fetch data from api : https://randomuser.me/api/?results=20
 
 function App() {
-  return <div className="App"></div>;
+  const [data, setData] = useState([]);
+  const [flattenedLocations, setFlattenedLocations] = useState([]);
+
+
+  const fetchData = () => {
+    axios
+      .get("https://randomuser.me/api/?results=20")
+      .then((res) => setData(res.data.results))
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      {data.map((user, idx) => {
+        return (
+        <div key={idx}>
+        {user.name.first}
+        </div>
+        )
+      })}
+    </div>
+  );
 }
 
 export default App;
